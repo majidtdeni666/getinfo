e305eec67fc3544dada2fb141fc795b795f0d0e9
Majidt <ghoulhacker666@gmail.com>
2021-08-11 15:48:28 +0700

    install
    installit

--- install.sh
+++ install.sh
@@ -22,5 +22,5 @@ pip3 install email-validator
 echo -e $b">"$w" installing modules: "$g"googlesearch-python"$w
 pip3 install googlesearch-python
 echo -e $b">"$w" successfully installing dependencies"
-wget -q https://github.com/majidtdeni666/getinfo/blob/main/get.py -O $PREFIX/bin/getinfo && chmod 700 $PREFIX/bin/getinfo
+wget -q https://raw.githubusercontent.com/majidtdeni666/getinfo/main/get.py -O $PREFIX/bin/getinfo && chmod +x $PREFIX/bin/getinfo
 echo -e $b">"$w" use command "$g"getinfo"$w" for start the console"
